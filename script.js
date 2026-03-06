a1a2fc83b4a060cd14ed77f8c85f4ecaa774a4ce
NafisaTabassum01 <tabassumnafisa28.01@gmail.com>
2026-03-06 22:14:33 +0600

    login section done

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("signIn-btn").addEventListener("click",function(){
+    const inputUserName=document.getElementById("input-username");
+    const userName=inputUserName.value;
+console.log(userName)
+
+
+    const inputPassword=document.getElementById("input-password");
+    const pin=inputPassword.value
+console.log(pin)
+
+if(userName=="admin" && pin=="admin123"){
+window.location.assign("/home.html")
+}
+else{
+    alert("Login failed");
+    return;
+}
+
+})
